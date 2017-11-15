0ace18c1f45e25bfe791afd86c397a838e793d77
s179065 <s179065@SEA-8E40F2>
2017-11-14 17:45:22 -0800

    Bleh

--- GameEngineExamples/main.cpp
+++ GameEngineExamples/main.cpp
@@ -9,7 +9,7 @@ bool doCollision(Player & player, const Wall & wall)
 
 	auto hit = collides(player.transform, player.collider,
 		wall.transform, wall.collider);
-
+	
 	if (hit.penetrationDepth > 0)
 	{
 		static_resolution(player.transform.position, player.rigidbody.velocity, hit);
